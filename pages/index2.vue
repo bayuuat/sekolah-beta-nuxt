732ac5df4a497e4f31f2bc28a3730d13b841fc20
bayuuat <bayuuat@gmail.com>
2022-04-25 23:46:07 +0700

    api

--- pages/index2.vue
+++ pages/index2.vue
@@ -28,9 +28,9 @@
       </div>
       <div class="list-task row">
         <CardItem
-          v-for="(iniisinya, i) in resultQuery"
+          v-for="(task, i) in tasks"
           :key="i"
-          :task="iniisinya"
+          :task="task"
           :isGrid="isGrid"
           :hide="hide"
         />
@@ -99,86 +99,31 @@ export default {
       // Tipe layout daftar task
       isGrid: false,
       form: {
+        id: 3,
         title: "",
         description: "",
+        isDone: false,
       },
-      tasks: [
-        {
-          title: "Brainstroming for new Ideas",
-          description: "ini deskripsi",
-          isDone: false,
-          category: "Manage",
-        },
-        {
-          title: "Approve and Assign ideas",
-          description: "ini deskripsi 2",
-          isDone: false,
-          category: "Manage",
-        },
-        {
-          title: "Work on design and UX",
-          description: " ini deskripsi 3",
-          isDone: false,
-          category: "Design",
-        },
-        {
-          title: "Publish and deploy",
-          description: "ini deskripsi 4",
-          isDone: false,
-          category: "Develop",
-        },
-        {
-          title: "Assign topics to writer",
-          description: "ini deskripsi 5",
-          isDone: false,
-          category: "Manage",
-        },
-        {
-          title: "Design layout",
-          description: " ini deskripsi 6",
-          isDone: false,
-          category: "Design",
-        },
-      ],
     };
   },
   computed: {
-    resultQuery() {
-      if (this.searchQuery) {
-        return this.$store.state.tasks.tasks.filter((item) => {
-          return this.searchQuery
-            .toLowerCase()
-            .split(" ")
-            .every((v) => item.title.toLowerCase().includes(v));
-        });
-      } else if (this.dropdownQuery) {
-        return this.$store.state.tasks.tasks.filter((item) => {
-          return this.dropdownQuery
-            .toLowerCase()
-            .split(" ")
-            .every((v) => item.category.toLowerCase().includes(v));
-        });
-      } else {
-        return this.$store.state.tasks.tasks;
-      }
-    },
-    uniqueCategory() {
-      return this.tasks.filter(
-        (value, index, self) =>
-          self.map((item) => item.category).indexOf(value.category) == index
-      );
+    tasks() {
+      return this.$store.state.tasks.tasks;
     },
   },
   methods: {
     handleSubmit() {
-      this.tasks.push({
-        title: this.form.title,
-        description: this.form.description,
+      this.form.id += 1;
+
+      var payload = this.form;
+
+      this.$store.dispatch("tasks/addTask", payload);
+
+      this.form = {
+        title: "",
+        description: "",
         isDone: false,
-        category: "Manage",
-      });
-      this.form.title = "";
-      this.form.description = "";
+      };
     },
   },
 };
